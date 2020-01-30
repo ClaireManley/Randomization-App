e38cb756f8890b33d4aabd20d9f2a9017c111ff4
Claire Manley <cmanley2@zagmail.gonzaga.edu>
2020-01-29 21:35:53 -0800

    basic randomization app

--- Randomization App/ViewController.swift
+++ Randomization App/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel1: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+  
+    @IBAction func magic8Pressed(_ sender: UIButton) {
+        let messages = [ "You will have a great day :)" , "You will have a bad day :( ", "You will have an average day :|" ]
+        messageLabel1.text = messages[Int.random(in: 0...messages.count-1)]
+    }
 }
+
 
